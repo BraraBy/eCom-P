0ab95f7aee5496a78a6705ea5eee802baab1b48a
BraraBy <natathapon.g@gmail.com>
2025-06-07 20:25:52 +0700

    Base API

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,14 +1,23 @@
-
-import React from 'react';
-
-const categories = [
-  { name: "Snack", color: "bg-blue-100" },
-  { name: "Drink", color: "bg-green-100" },
-  { name: "Household", color: "bg-red-100" },
-  { name: "Stationery", color: "bg-yellow-100" },
-];
+import { useEffect, useState } from "react";
 
 const Category = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+      fetch('http://localhost:4200/api/category')
+        .then(res => res.json())
+        .then(data => {
+          if (data.status === "200" && Array.isArray(data.result)) {
+            setCategories(data.result);
+          }
+        })
+        .catch(err => {
+          console.error('Failed to fetch products:', err);
+        });
+    }, []);
+
+
   return (
     <div className="flex justify-center gap-4 py-6">
       {categories.map((cat, index) => (
